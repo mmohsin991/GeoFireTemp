3f0fe1ab74e96da9840c295bd7235c8b876328b1
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-01-29 16:20:02 +0500

    My GeoFire Project

--- GeoFireTemp/ViewController.swift
+++ GeoFireTemp/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
+let ref = Firebase(url: "https://geofiretemp.firebaseio.com/")
+let geoFire = GeoFire(firebaseRef: ref)
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        var arr = ["mohsin":"Muhammad Mohsin"]
+        ref.updateChildValues(arr)
+        
+        geoFire.setLocation(CLLocation(latitude: 37.7853889, longitude: -122.4056973), forKey: "firebase-hq") { (error) in
+            if (error != nil) {
+                println("An error occured: \(error)")
+            } else {
+                println("Saved location successfully!")
+            }
+        }
+        
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
