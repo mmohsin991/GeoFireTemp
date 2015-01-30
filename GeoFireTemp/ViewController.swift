1b93a06037cda1539056eee8ca9833d70baac661
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-01-30 19:56:49 +0500

    update

--- GeoFireTemp/ViewController.swift
+++ GeoFireTemp/ViewController.swift
@@ -14,6 +14,8 @@ let geoFire = GeoFire(firebaseRef: ref)
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMsg: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +24,7 @@ class ViewController: UIViewController {
         var arr = ["mohsin":"Muhammad Mohsin"]
         ref.updateChildValues(arr)
         
+        // set the location
         geoFire.setLocation(CLLocation(latitude: 37.7853889, longitude: -122.4056973), forKey: "firebase-hq") { (error) in
             if (error != nil) {
                 println("An error occured: \(error)")
@@ -30,7 +33,58 @@ class ViewController: UIViewController {
             }
         }
         
+        // remove the location
+        //geoFire.removeKey("firebase-hq")
+
         
+        // get the location
+        geoFire.getLocationForKey("firebase-hq", withCallback: { (location, error) in
+            if (error != nil) {
+                println("An error occurred getting the location for \"firebase-hq\": \(error.localizedDescription)")
+            } else if (location != nil) {
+                println("Location for \"firebase-hq\" is [\(location.coordinate.latitude), \(location.coordinate.longitude)]")
+            } else {
+                println("GeoFire does not contain a location for \"firebase-hq\"")
+            }
+        })
+        
+        
+        
+        // geo fire query 
+        
+        let center = CLLocation(latitude: 37.7832889, longitude: -122.4056973)
+        // Query locations at [37.7832889, -122.4056973] with a radius of 600 meters
+        var circleQuery = geoFire.queryAtLocation(center, withRadius: 0.6)
+        
+        
+        
+        // Query location by region
+        let span = MKCoordinateSpanMake(0.001, 0.001)
+        let region = MKCoordinateRegionMake(center.coordinate, span)
+        var regionQuery = geoFire.queryWithRegion(region)
+        
+        
+        var queryHandle1 = circleQuery.observeEventType(GFEventTypeKeyEntered, withBlock: { (key: String!, location: CLLocation!) in
+            let msg = "Key '\(key)' entered in the search area and is at location: '\(location)'"
+            println(msg)
+            self.updateLabel(msg)
+        })
+        
+        var queryHandle2 = circleQuery.observeEventType(GFEventTypeKeyExited, withBlock: { (key: String!, location: CLLocation!) in
+            let msg = "Key '\(key)'\n exacted in the search area and is at location: '\(location)'"
+            println(msg)
+            self.updateLabel(msg)
+
+        })
+        
+//        var queryHandle3 = circleQuery.observeEventType(GFEventTypeKeyExited, withBlock: { (key: String!, location: CLLocation!) in
+//            let msg = "Key '\(key)'\n move in the search area and is at location: '\(location)'"
+//            println(msg)
+//            self.updateLabel(msg)
+//
+//        })
+        
+
     }
     
 
@@ -39,6 +93,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func updateLabel (text: String) {
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.lblMsg.text = text
+        })
+
+        
+
+
+    }
 
 }
 
